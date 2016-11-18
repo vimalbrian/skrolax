5723aa28d0f76ea153be37d5de0ba58e8fc0b139
vimalbrian <vimalbrian@gmail.com>
2016-11-18 08:23:13 +0530

    New development

---
+++ assets/js/skrolax.js
@@ -0,0 +1,28 @@
+var skrolax = {
+	init:function(){
+		$(window).on('load scroll', function(){
+			$('.parallax').each(function(){
+				this_elem = $(this);
+				
+				bgPos = $(this).css('background-position').split(' ');
+				xPos = bgPos[0];
+				
+				if($(this).data('speed') <= 1.0){
+					yPos = parseInt(($(this).offset().top-$(window).scrollTop())*$(this).data('speed'));
+				}
+				else{
+					yPos = parseInt(($(this).offset().top-$(window).scrollTop())*1.0);
+				}
+				
+				$(this).css('background-position', xPos+' '+yPos+'px');
+			});
+		});
+	},
+	scroll:function(){
+	}
+};
+
+
+$(document).ready(function(){
+	skrolax.init();
+});
